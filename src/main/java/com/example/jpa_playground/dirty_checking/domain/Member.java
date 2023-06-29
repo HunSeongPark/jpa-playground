5d44a509becd398de8082173765c76294946454f
HunSeongPark <phunseong@gmail.com>
2023-06-29 19:33:13 +0900

    feat: Member entity 추가

---
+++ src/main/java/com/example/jpa_playground/dirty_checking/domain/Member.java
@@ -0,0 +1,20 @@
+package com.example.jpa_playground.dirty_checking.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private Integer age;
+}
